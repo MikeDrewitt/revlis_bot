5c36cf9b0b0f28c558009cbd2fb82ffab65be429
Michael Drewitt <mikedrewitt@gmail.com>
2016-10-21 19:13:35 -0400

    finding bots channels done

--- slack/revlis.py
+++ slack/revlis.py
@@ -9,12 +9,11 @@ from slackclient import SlackClient
 from pprint import pprint
 
 BOT_NAME = 'silver'
-DATABASE = '../db/db.json'
 
-slack_client = SlackClient('xoxp-23584478294-23583487255-94654266630-563d319952ec7a47e0d8b25b38d534de')
+CHANNEL_ARRAY = []
+
+SLACK_CLIENT = SlackClient('xoxp-23584478294-23583487255-94654266630-563d319952ec7a47e0d8b25b38d534de')
 BOT_ID = 'U2D8VMVQS'
-
-# constants
 AT_BOT = "<@" + BOT_ID + ">"
 
 
@@ -30,14 +29,54 @@ def is_int(val):
         #print("Val is false: " + val)
         return False
 
+def get_bot_channels(slack_client, bot_id):
+
+    channel_list = slack_client.api_call('channels.list')
+    group_list = slack_client.api_call('groups.list')
+
+    bot_channels = []
+
+    #print(slack_client.api_call('channels.info', channel="C0VN9R41F"))
+
+    for channel in channel_list['channels']:
+
+        if not channel['is_archived']:
+            channel_id = channel['id']
+
+            channel_info = slack_client.api_call('channels.info', channel=channel_id)
+
+            #print(channel_info)
+
+            for member in channel_info['channel']['members']:
+                if BOT_ID == member:
+                    #print('found him! in ' + channel_info['channel']['name'])
+                    bot_channels.append(channel_id)
+
+
+    for group in group_list['groups']:
+
+        if not group['is_archived']:
+            group_id = group['id']
+            group_info = slack_client.api_call('groups.info', channel=group_id)
+
+            #print(group_info)
+
+            for member in group_info['group']['members']:
+                if BOT_ID == member:
+                    #print('found him! in ' + group_info['group']['name'])
+                    bot_channels.append(group_id)
+
+    return bot_channels
+
 def slack_commands_list(command, channel):
     response = "Not sure what you mean. Try again later."
 
     #This is going to hold the room by updating the json object at paramerterized time and day
-    if command.startswith("hullo"):
-        slack_client.api_call("chat.postMessage", channel=channel, text="I LIVE!", as_user=True)
+    if command.startswith("refresh"):
+        CHANNEL_ARRAY = get_bot_channels(SLACK_CLIENT, BOT_ID)
+        SLACK_CLIENT.api_call("chat.postMessage", channel=channel, text="Channel list has been refreshed!", as_user=True)
     else:
-        slack_client.api_call("chat.postMessage", channel=channel, text="No command found.", as_user=True)
+        SLACK_CLIENT.api_call("chat.postMessage", channel=channel, text="No command found.", as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -51,70 +90,40 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return true, and all data info after     
+                # return true, and all data info after
                output['text'] = output['text'].split(AT_BOT)[1].strip().lower()
                return True, output
 
-            if output and 'text' in output: 
+            if output and 'text' in output:
                # return false and all data info
                return False, output
 
     return None, None
 
-def get_bot_channels(slack_client, bot_id):
-            
-    channel_list = slack_client.api_call('channels.list')
-    group_list = slack_client.api_call('groups.list')
-        
-    #print(slack_client.api_call('channels.info', channel="C0VN9R41F"))
-
-    for channel in channel_list['channels']:
-        channel_id = channel['id']
-      
-        channel_info = slack_client.api_call('channels.info', channel=channel_id)         
-
-        print(channel_info)
-                
-        if channel_info['ok'] == True:
-            for g in channel_info['channel']:
-                print(g)
-                        
-                #if BOT_ID in g['members']:
-                #print('found him!')
-
-
-    for group in group_list['groups']:
-        group_id = group['id']
-        group_info = slack_client.api_call('groups.info', channel=group_id)
-             
-        print(group_info)
-                
-        if group_info['ok'] == True:
-            for g in group_info['group']:
-                print(g)                
-
-
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
+    if SLACK_CLIENT.rtm_connect():
         print("Silver connected and running!")
 
+        CHANNEL_ARRAY = get_bot_channels(SLACK_CLIENT, BOT_ID)
+
+        print(CHANNEL_ARRAY)
+  
         while True:
 
-            at_bot, chat_dictionary = parse_slack_output(slack_client.rtm_read())
-
-
+            at_bot, chat_dictionary = parse_slack_output(SLACK_CLIENT.rtm_read())
+            
             if at_bot != None:
                 channel = chat_dictionary['channel']
                 command = chat_dictionary['text']
-  
-                if at_bot:  
+
+                if at_bot:
                     slack_commands_list(command, channel)
-                else:          
-                    print(command) 
+                else:
+                    print(command)
             else:
-                a = 1 
+                a = 1
 
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
